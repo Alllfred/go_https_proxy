3a708c234bc760b4650adee013c10c0ea3a03cf0
zheng.li <zheng.li@immomo.com>
2020-06-11 15:37:23 +0800

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_https_proxy
 
 go 1.13
+
+require github.com/elazarl/goproxy v0.0.0-20200426045556-49ad98f6dac1
--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"github.com/elazarl/goproxy"
 	"log"
 	"net/http"
-
-	"github.com/elazarl/goproxy"
 )
 
 func main() {
-    proxy := goproxy.NewProxyHttpServer()
+	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-	proxy.OnRequest().DoFunc(
-    func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
-		fmt.Printf("ok!!")
-        return r,nil
-    })
-    log.Fatal(http.ListenAndServe(":8080", proxy))
+	proxy.OnRequest(goproxy.DstHostIs("www.bilibili.com")).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			ctx.Logf("req = %v", r)
+			return r, nil
+		})
+	log.Fatalln(http.ListenAndServe(":8080", proxy))
 }
- 
